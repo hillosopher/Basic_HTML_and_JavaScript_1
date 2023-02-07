41be82d03ef9b157d209b44a2f708548758ad0a4
Josh Hill <joshuaamhill@gmail.com>
2023-02-06 21:18:30 -0800

    Init'ed this dir. Created Basic HTML 1 and 2, as well as the accompanying Script file.

---
+++ Basic_JavaScript_1.js
@@ -0,0 +1,8 @@
+"use strict";
+alert("Yoooo!");
+
+function My_First_Function() {
+  let String = "Kiss me, I'm Irish!";
+  let result = String.fontcolor("green");
+  document.getElementById("Irish").innerHTML = result;
+}
